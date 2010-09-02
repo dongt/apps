850eb94e13d4ccc4ac9d4515dd38297c7cf59d6a
rockydd <rockydd@gmail.com>
2010-09-02 05:11:39 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanfan::Application.config.secret_token = '0cba3dad0d61d522aaffb106d81695777d042a1e0300f1e768462b2374129f261d7052affec028beecb99de45db0dc12ca39b2a18c7d130873fa89aa664574a4'
