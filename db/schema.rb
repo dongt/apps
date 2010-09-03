23613cbbbece33a8407873ecdf2ad7e7af21c495
rockydd <rockydd@gmail.com>
2010-09-02 23:41:07 -0400

    added model participation

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100903032802) do
+ActiveRecord::Schema.define(:version => 20100903034025) do
 
   create_table "activities", :force => true do |t|
     t.string   "subject"
     t.string   "status"
     t.string   "detail"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "participations", :force => true do |t|
+    t.integer  "activity_id"
+    t.integer  "user_id"
+    t.boolean  "confirmed"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
