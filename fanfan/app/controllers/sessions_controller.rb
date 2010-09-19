b81c1c51d7fe0dbe2a1b0902e7b2d11014f6561e
Shawn <shawn.x.z@gmail.com>
2010-09-19 17:34:32 +0000

    change redirect page to activities#index

--- fanfan/app/controllers/sessions_controller.rb
+++ fanfan/app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     user = User.authenticate(params[:login], params[:password])
     if user
       session[:user_id] = user.id
       flash[:notice] = "Logged in successfully."
-      redirect_to_target_or_default("/")
+      redirect_to :controller => :activities, :action => :index
     else
       flash.now[:error] = "Invalid login or password."
       render :action => 'new'
     end
   end
-  
+
   def destroy
     session[:user_id] = nil
     flash[:notice] = "You have been logged out."
