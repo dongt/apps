2743ee967acd658d5c5e113b7df6453dc4223ed8
rockydd <rockydd@gmail.com>
2010-09-19 15:33:51 +0800

    payments part almost done. ajax add new payer done.

--- fanfan/app/controllers/activities_controller.rb
+++ fanfan/app/controllers/activities_controller.rb
@@ -2,20 +2,40 @@ class ActivitiesController < ApplicationController
   def index
     @activities = Activity.all
   end
-  
+
   def show
     @activity = Activity.find(params[:id])
   end
-  
+
   def new
     @activity = Activity.new
   end
-  
+
   def create
-    usernames = params[:activity].delete(:user_names).split(",")
-    users = usernames.map{|n| User.find_by_name(n)}
-    params[:activity][:users] = users
+    #usernames = params[:activity].delete(:user_names).split(",")
+    #users = usernames.map{|n| User.find_by_name(n)}
+    #params[:activity][:users] = users
+    cost=0
+    payers_params=params.keys.find_all{|key| key =~ /^payer\d+/}
+    if(payers_params)
+      payments = payers_params.map do |namek|
+        i=namek.match(/payer(\d+)/)[1]
+        amountk="payamount#{i}"
+        name=params[namek];
+        amount=params[amountk]
+        user=User.find_by_name(name)
+        payment=Payment.new(:amount=>amount)
+        payment.user=user;
+        payment.amount=amount;
+        cost += payment.amount
+        payment
+      end
+      params[:activity][:payments] = payments
+    end
+
+
     @activity = Activity.new(params[:activity])
+    @activity.cost=cost
     if @activity.save
       flash[:notice] = "Successfully created activity."
       redirect_to @activity
@@ -23,11 +43,11 @@ class ActivitiesController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def edit
     @activity = Activity.find(params[:id])
   end
-  
+
   def update
     @activity = Activity.find(params[:id])
     usernames = params[:activity].delete(:user_names).split(",")
@@ -40,7 +60,7 @@ class ActivitiesController < ApplicationController
       render :action => 'edit'
     end
   end
-  
+
   def destroy
     @activity = Activity.find(params[:id])
     @activity.destroy
--- fanfan/app/models/activity.rb
+++ fanfan/app/models/activity.rb
@@ -3,8 +3,8 @@ class Activity < ActiveRecord::Base
   has_many :users, :through => :participations
   has_many :payers, :through => :payments, :source => :user
   has_many :payments
-  attr_accessible :subject, :status, :detail, :users, :cost
-  
+  attr_accessible :subject, :status, :detail, :users, :cost, :payers, :payments
+
 
   def user_names
     return "" if users.empty?
