7a7853c4f9756b6a7f26c194f0a9a96dd0eb6b03
rockydd <rockydd@gmail.com>
2010-10-08 01:47:39 +0800

    fixed bug of user name in show activity

--- fanfan/app/models/activity.rb
+++ fanfan/app/models/activity.rb
@@ -1,16 +1,15 @@
 class Activity < ActiveRecord::Base
   has_many :participations
-  has_many :users, :through => :participations
   has_many :payers, :through => :payments, :source => :user
   has_many :payments
   validates_presence_of :subject
   validates_presence_of :status
   validates_presence_of :cost
-  attr_accessible :subject, :status, :detail, :users, :cost, :payers, :payments
+  attr_accessible :subject, :status, :detail, :cost, :payers, :payments
 
 
-  def user_names
-    return "" if users.empty?
-    return users.map{|u| u.name}.join(",")
+  def payer_names
+    return "" if payers.empty?
+    return payers.map{|u| u.username}.join(",")
   end
 end
